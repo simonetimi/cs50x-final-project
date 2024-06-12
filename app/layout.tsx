b7b7f4fdaec14323c444c3d524c171ad51162afe
Simone <150595274+simonetimi@users.noreply.github.com>
2024-06-12 23:06:11 +0200

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import { NextUIProvider } from '@nextui-org/react';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'CS50 Final Project',
+  description: 'Project for the CS50 course',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <NextUIProvider>
+        <body className={inter.className}>{children}</body>
+      </NextUIProvider>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+export default function Home() {
+  // server component to call quiz api
+  // set it to a state
+
+  // set states to handle: username, correct answers, incorrect, best scores, difficulty setting, current question/total
+  // save to local storage on click
+  // keep the highest score with difficulty
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Hi
+    </main>
+  );
+}
